182660ae2e0ada1b6087ae2f956dc3fc5d400f1d
Dedito Cantila <dedito.cantila@gmail.com>
2017-03-17 21:59:03 +0800

    feature(ui): made progress with categories and homepage

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,109 +1,134 @@
+'use strict';
 // Let's register Template7 helper so we can pass json string in links
-Template7.registerHelper('json_stringify', function (context) {
-    return JSON.stringify(context);
+Template7.registerHelper( 'json_stringify', function ( context ) {
+	return JSON.stringify( context );
 });
 
 // Initialize your app
-var myApp = new Framework7({
-    animateNavBackIcon: true,
-    // Enable templates auto precompilation
-    precompileTemplates: true,
-    // Enabled pages rendering using Template7
-    template7Pages: true,
-    swipePanel:'left',
-    // Specify Template7 data for pages
-    template7Data: {
-        // Will be applied for page with "projects.html" url
-        'url:projects.html': {
-            firstname: 'John',
-            lastname: 'Doe',
-            age: 32,
-            position: 'CEO',
-            company: 'Google',
-            interests: ['swimming', 'music', 'JavaScript', 'iMac', 'iOS apps', 'sport'],
-            projects: [
-                {
-                    title: 'Google',
-                    description: 'Nice search engine'
-                },
-                {
-                    title: 'YouTube',
-                    description: 'Online video service'
-                },
-                {
-                    title: 'Android',
-                    description: 'Mobile operating system'
-                }
-            ]
-        },
+var myApp = new Framework7( {
+	'animateNavBackIcon'  : true,
+	'template7Pages'      : true,
+	'swipePanel'          :'both',
+	'precompileTemplates' : true
+} );
 
-        // Will be applied for page with data-page="contacts"
-        'page:contacts': {
-            tel: '+1 (222) 333-44-55',
-            email: 'john@doe.com',
-            country: 'USA',
-            city: 'San Francisco',
-            zip: '12345',
-            street: 'Awesome st'
-        },
-
-        // Just plain data object that we can pass for other pages using data-contextName attribute
-        cars: [
-            {
-                vendor: 'Volkswagen',
-                model: 'Passat',
-                power: 152,
-                speed: 280,
-                weight: 1400,
-                color: 'black',
-                year: 2012,
-                description: ''
-            },
-            {
-                vendor: 'Skoda',
-                model: 'Superb',
-                power: 152,
-                speed: 260,
-                weight: 1600,
-                color: 'white',
-                year: 2013,
-                description: ''
-            },
-            {
-                vendor: 'Ford',
-                model: 'Mustang',
-                power: 480,
-                speed: 320,
-                weight: 1200,
-                color: 'red',
-                year: 2014,
-                description: ''
-            },
-        ],
-
-        // Another plain data object, used in "about" link in data-contextName object 
-        about: {
-            name: 'John Doe',
-            age: 32,
-            position: 'CEO',
-            company: 'Google',
-            interests: ['swimming', 'music', 'JavaScript', 'iMac', 'iOS apps', 'sport']
-        }
-    }
-});
+var gd = {};
 
 // Export selectors engine
 var $$ = Dom7;
 
-// Add main View
-var mainView = myApp.addView('.view-main', {
-    // Enable dynamic Navbar
-    dynamicNavbar: true,
-});
+// Add Views
+var mainView = myApp.addView( '.view-main', {} );
+var left  = myApp.addView( '.view-left', {} );
 
-var mySearchbar = $$('.searchbar')[0].f7Searchbar;
+// show page loader while fetching data
+mainView.router.load( {
+	'template'     : myApp.templates.pageLoader,
+	'animatePages' : false,
+	'reload'        : true
+} );
+
+function sort ( array, key ) {
+	return ( array || [] ).sort( function( a, b ) {
+   		if ( a[ key ] < b[ key ] ) return -1;
+   		if ( a[ key ] > b[ key ] ) return 1;
+    	return 0;
+	} );
+}
+
+function constructQuery ( query ) {
+	return 'where=' + encodeURIComponent( query.where ) + '&'
+		+ 'sort=' + encodeURIComponent( query.sort ) + '&'
+		+ 'offset=' + encodeURIComponent( query.offset || 0 ) + '&'
+		+ 'limit=' + encodeURIComponent( query.limit || 100 );
+}
+
+function getCategories ( callback ) {
+	$$.ajax( {
+		'dataType' : 'json',
+		'url'      : 'http://52.34.60.86:3000/categories/GD:getCategories' ,
+		
+		'success' : function searchSuccess( res ) {
+			callback( sort( res, 'categoryname' ) );
+		},
+
+		error : function searchError( xhr, err ) {
+			( callback || function () {} )( null, err );
+		}
+	} );
+}
+
+function getProducts ( query ) {
+	return new Promise( function ( resolve, reject ) {
+		$$.ajax( {
+			'dataType' : 'json',
+			'url'      : 'http://52.34.60.86:3000/products?' + constructQuery( query ),
+			
+			'success' : function searchSuccess( res ) {
+				resolve( res );
+			},
+
+			error : function searchError( xhr, err ) {
+				reject( err );
+			}
+		} );
+	} );
+}
+
+function loadGDHomepage () {
+	var homeData = gd.categories.map( function ( category ) {
+		var query = {
+			'where'  : 'category=' + category.categoryname,
+			'sort'   : 'popularity',
+			'offset' : 0,
+			'limit'  : 20
+		};
+
+		return getProducts( query );
+	} );
+
+	Promise
+		.all( homeData )
+		.then( function ( products ) {
+			mainView.router.load( {
+				'template'     : myApp.templates.homePage,
+				'reload'       : true,
+				'context' : {
+					'categories' : gd.categories.map( function ( item, index ) {
+						item.products = products[ index ];
+						return item;
+					} )
+				}
+			} );
+		} )
+		.catch( function ( err ) {
+			myApp.alert( err );
+		} );
+}
+
+function loadSidebar ( categories ) {
+	gd.categories = categories || [];
+
+	left.router.load( {
+		'template'     : myApp.templates.sideBarTpl,
+		'animatePages' : false,
+		'reload'       : true,
+		'context'      : {
+			'categories' : categories
+		}
+	} );
+
+	loadGDHomepage();
+}
+
+function initView () {
+	getCategories( loadSidebar );
+}
+
+initView();
 
 $$(document).on('submit', '.searchbar', function (e) { 
-  var formData = myApp.formToJSON('.searchbar');
-  myApp.alert( formData.q );
+	var formData = myApp.formToJSON('.searchbar');
+	myApp.alert( formData.q );
 });
+
